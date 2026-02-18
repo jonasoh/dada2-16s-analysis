a3c945a0f9e5ac8a280d2b73fd9d348dd56b07a8
Jonas Ohlsson <jonas@vansinne.se>
2026-02-18 16:02:22 +0100

    allow processing single samples (fixes #2)

--- dada2.R
+++ dada2.R
@@ -89,7 +89,7 @@ if (!exists('output_dir')) output_dir <- file.path(input_dir, "DADA2_output/")
 # use multithreading only if we aren't on windows
 multithread <- if (.Platform$OS.type == "windows") FALSE else n_cores
 
-dir.create(output_dir)
+dir.create(output_dir, showWarnings=FALSE)
 
 # write some basic info to log file
 logfile <- file.path(output_dir, 'dada2_output.txt')
@@ -166,10 +166,12 @@ dadaRs <- dada(rev_filt, err=errR, multithread=multithread)
 
 # merge the paired ends
 mergers <- mergePairs(dadaFs, fwd_filt, dadaRs, rev_filt, verbose=TRUE)
-names(mergers) <- sample.names
+if (length(sample.names) > 1) names(mergers) <- sample.names
 
 # make the asv table
 seqtab <- makeSequenceTable(mergers)
+if (length(sample.names) == 1) rownames(seqtab) <- sample.names
+
 seqtab.nochim <- removeBimeraDenovo(seqtab, method="consensus", multithread=multithread, verbose=TRUE)
 
 # check reads throughout the process
